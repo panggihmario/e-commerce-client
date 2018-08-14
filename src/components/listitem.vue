35f4af454543bd177310fade71d3535738f7af7e
mario <panggihmario@gmail.com>
2018-08-15 06:31:30 +0700

    fix grid

--- src/components/listitem.vue
+++ src/components/listitem.vue
@@ -1,42 +1,42 @@
 <template>
-    <v-container grid-list-md text-xs-center >
-	 <v-layout >
-      <v-flex xs4 order-md3 order-xs2 v-for="(item,index) in items" :key=index class="card">
-        <v-card id="cards"> 
-            <center>
-          <v-card-media class="image"
-            :src="item.url"
-            height="290px"
-          ></v-card-media>
-            </center>
-          <v-card-title primary-title>
-              
-            <div>
-              <h3 class="text-md-right">{{item.name}}</h3>
-              <div>{{item.price}} <br>{{item.category}}</div>
-            </div>
-          </v-card-title>
-  
-          <v-card-actions>
-            <v-btn flat color="orange">Share</v-btn>
-            <v-btn flat color="orange">Explore</v-btn>
-          </v-card-actions>
-         
+    <v-container grid-list-md text-xs-center> 
+         <v-layout row wrap>
+        	<v-flex  xs12 sm4 md3  v-for="(item,index) in items" :key=index >
+         <v-card >
+				<v-card-media
+					:src="item.url"
+					height="190px"
+				></v-card-media>
+				<v-card-title primary-title>
+					<div>
+					<v-list two-line subheader>
+					<v-subheader inset>
+						<h2> {{item.name}}
+						</h2>
+					</v-subheader>
+					
+					<v-list-tile-content>
+						<v-list-tile-title>{{ item.category }}</v-list-tile-title>
+						<v-list-tile-sub-title> Rp {{ item.price }}</v-list-tile-sub-title>
+					</v-list-tile-content>
+					
+					</v-list>
+					</div>
+				</v-card-title>
+				<v-card-actions>
+					<v-btn outline color="indigo" v-on:click="addToCart(item)">Add to Cart</v-btn>
+					
+				</v-card-actions>
         </v-card>
-      </v-flex>
-    </v-layout>
-	</v-container>
+        </v-flex>
+          
+        
+       
+         </v-layout>
+</v-container>
 </template>
 
-<style scoped>
-    .image{
-        width: 300px;
-        
-    };
-    .cards{
-        width: 500px
-    }
-</style>
+
 
 
 
@@ -59,3 +59,18 @@ export default {
 	}
 }
 </script>
+
+<style lang="scss">
+.v-card {
+    display: block;
+    border-radius: 5px;
+    border: solid 2px;  
+    min-width: 0;
+    position: relative;
+    text-decoration: none;
+    padding :-1px;
+    margin :6px
+    
+    
+}
+</style>
